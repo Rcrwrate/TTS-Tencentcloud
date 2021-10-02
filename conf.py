ddf736fd53e646ba89d1c07847e55534e1db6a9e
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2021-10-02 14:28:34 +0800

    conf修正

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 from msg import msg
+import sys
 
 def conf():
     print("="*50 + "\n作者是崩坏学园2官方测试服群组2群或者3群的红贽人形哒\n" + "="*50 + "\n")
@@ -15,6 +16,7 @@ def conf():
         print("文件conf.txt不存在,已自动生成模板文件，请按照标准填写完成后再次运行本程序")
         print("请仔细阅读说明文档：readme.txt")
         input("按回车键退出")
+        sys.exit()
     except SyntaxError:
         input("conf.txt文件异常，删除或者修正\n按回车键退出")
     else:
